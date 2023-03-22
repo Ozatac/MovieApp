690d3c1fb46ea4fbd479f04cbf9c25c6425d0346
Tunahan Ozatac <exttunahano@d-teknoloji.com.tr>
2023-03-22 16:12:48 +0300

    Api method Constants.kt taşındı.

--- app/src/main/java/com/tunahanozatac/movieapp/data/network/ApiServices.kt
+++ app/src/main/java/com/tunahanozatac/movieapp/data/network/ApiServices.kt
@@ -2,13 +2,14 @@ package com.tunahanozatac.movieapp.data.network
 
 import com.tunahanozatac.movieapp.domain.model.DetailsModel.MovieDetailsModel
 import com.tunahanozatac.movieapp.domain.model.NowPlayingModel.MovieResponse
+import com.tunahanozatac.movieapp.util.Constants.NowPlayingMovies
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiServices {
-    @GET("movie/now_playing?page=1")
+    @GET(NowPlayingMovies)
     suspend fun getNowPlayingMovies(): MovieResponse
 
     @GET("movie/upcoming")
--- app/src/main/java/com/tunahanozatac/movieapp/util/Constants.kt
+++ app/src/main/java/com/tunahanozatac/movieapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/original"
     const val IMDB_REDIRECT_URL = "https://www.themoviedb.org/redirect?external_source=imdb_id&external_id="
+    const val NowPlayingMovies = "movie/now_playing?page=1"
 }
